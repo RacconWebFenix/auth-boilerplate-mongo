2f8b55d84677469774582845f0b4fca73e96acd9
Marcos César Domingues <dfenixweb@gmail.com>
2022-11-10 10:20:38 -0300

    simple crud implements in users

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,18 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop()
+  name: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
