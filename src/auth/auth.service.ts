8621bbbafc477d9a9c53a97720df778a69cc06fd
Marcos César Domingues <dfenixweb@gmail.com>
2022-11-12 10:04:02 -0300

    JWT injects

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,20 +3,22 @@ import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { User } from 'src/user/entities/user.entity';
 import { UserPayload } from './models/UserPayload';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   login(user: User) {
-  
     const payload: UserPayload = {
       sub: user.id,
       email: user.email,
       name: user.name,
     };
 
-    
     console.log(user);
   }
 
