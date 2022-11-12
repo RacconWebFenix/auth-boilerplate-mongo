38bea9acdc605302775870ccba3ed65c938eb50c
Marcos César Domingues <dfenixweb@gmail.com>
2022-11-12 10:11:21 -0300

    access_token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import * as bcrypt from 'bcrypt';
 import { User } from 'src/user/entities/user.entity';
 import { UserPayload } from './models/UserPayload';
 import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/UserToken';
 
 @Injectable()
 export class AuthService {
@@ -12,14 +13,17 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
-  login(user: User) {
+  login(user: User): UserToken {
     const payload: UserPayload = {
       sub: user.id,
       email: user.email,
       name: user.name,
     };
+    const jwtToken = this.jwtService.sign(payload);
 
-    console.log(user);
+    return {
+      access_token: jwtToken,
+    };
   }
 
   async validateUser(email: string, password: string) {
